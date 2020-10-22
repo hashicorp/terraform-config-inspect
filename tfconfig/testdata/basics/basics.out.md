5bf370f38b1653e154b283b349e268e5f7e76e23
Andy Caruso <carusom@verily.com>
2020-10-22 13:53:54 -0700

    Support the sensitive attribute on outputs (#55)
    If sensitive is not specified or is false it will not be present
    in the Output object as per omitempty behavior.

--- tfconfig/testdata/basics/basics.out.md
+++ tfconfig/testdata/basics/basics.out.md
@@ -7,12 +7,15 @@ Provider Requirements:
 ## Input Variables
 * `A` (default `"A default"`)
 * `B` (required): The B variable
+* `C` (required): The C variable
 
 ## Output Values
 * `A`
 * `B`: I am B
+* `C`: C is sensitive
 
 ## Managed Resources
 * `null_resource.A` from `null`
 * `null_resource.B` from `null`
+* `null_resource.C` from `null`
 
--- tfconfig/testdata/basics/basics.tf
+++ tfconfig/testdata/basics/basics.tf
@@ -6,6 +6,10 @@ variable "B" {
     description = "The B variable"
 }
 
+variable "C" {
+    description = "The C variable"
+}
+
 output "A" {
     value = "${var.A}"
 }
@@ -13,7 +17,15 @@ output "A" {
 output "B" {
     description = "I am B"
     value = "${var.A}"
+    sensitive = false
+}
+
+output "C" {
+    description = "C is sensitive"
+    value = "${var.C}"
+    sensitive = true
 }
 
 resource "null_resource" "A" {}
 resource "null_resource" "B" {}
+resource "null_resource" "C" {}
