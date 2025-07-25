84b8dbeedf480e1fc949193b813c2c8e529acdfc
Lion Chen <lion.chen@hashicorp.com>
2025-07-25 17:53:44 -0600

    remove version from  component

--- tfconfig/component.go
+++ tfconfig/component.go
@@ -5,8 +5,7 @@ package tfconfig
 
 // Component represents a single component from a Terraform stack config.
 type Component struct {
-	Name    string    `json:"name"`
-	Source  string    `json:"source"`
-	Version string    `json:"version,omitempty"`
-	Pos     SourcePos `json:"pos"`
+	Name   string    `json:"name"`
+	Source string    `json:"source"`
+	Pos    SourcePos `json:"pos"`
 }
