1c5d6049da7dc1f90812883a0d58ce53c935e373
Glenn Sarti <glenn.sarti@hashicorp.com>
2026-02-23 14:35:13 +0800

    Only warn when the post init files are missing
    Previously the inspection would raise daignostic errors when the module
    or provider manifests were missing, however this is not correct. It is perfectly
    valid for an initialized configuration to not contain any modules or providers
    and therefore there is no manifest. This commit checks the error and returns
    a warning on FileNotExist errors. This commit also adds tests for this
    scenario

--- tfconfig/load_post_init_test.go
+++ tfconfig/load_post_init_test.go
@@ -70,6 +70,23 @@ func TestLoadPostInit(t *testing.T) {
 	}
 }
 
+func TestLoadPostInit_Empty(t *testing.T) {
+	path := filepath.Join("testdata-post-init", "empty")
+	dataDir := filepath.Join(path, ".terraform")
+
+	cfg := LoadPostInit(path, dataDir)
+
+	if cfg == nil {
+		t.Fatal("result is nil")
+	}
+
+	assertEqual(t, cfg.Path, path, "path")
+	assertEqual(t, len(cfg.Providers), 0, "providers count")
+	assertEqual(t, len(cfg.Modules), 0, "modules count")
+	assertEqual(t, cfg.Diagnostics.HasErrors(), false, "diagnostic errors")
+	assertEqual(t, len(cfg.Diagnostics), 2, "diagnostics count")
+}
+
 func TestLoadPostInitFromFilesystem(t *testing.T) {
 	path := filepath.Join("testdata-post-init", "basic")
 	dataDir := filepath.Join(path, ".terraform")
